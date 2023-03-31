93a6afacefae65bda44a2ca9a9a44065a2e8312f
phucciTr <miabam26@gmail.com>
2023-03-30 22:02:40 -0700

    Made change to schema to fix bug on the same title tasks

--- server/db/schema.js
+++ server/db/schema.js
@@ -10,7 +10,7 @@ module.exports = (db) => {
     return db.queryAsync(
       `CREATE TABLE IF NOT EXISTS tasks (
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(40) NOT NULL UNIQUE,
+        title VARCHAR(40) NOT NULL,
         description VARCHAR(250) NOT NULL,
         priority INT NOT NULL,
         dueDate INT NOT NULL,
